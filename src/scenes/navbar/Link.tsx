29a4e7e815346f0d6dc72b45b1fbbdbc15330ad1
unknown <jonas.gastro91@gmail.com>
2023-05-07 19:10:47 -0300

    feat: home screen

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -12,7 +12,7 @@ export default function Link({page, selectedPage, setSelectedPage}: Props) {
   const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
   return (
     <AnchorLink
-      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} transition duration-500 hover:text-prymary-300`}
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} transition duration-500 hover:text-primary-300`}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
     >
